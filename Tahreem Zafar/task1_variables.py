435f29c409aa74bc221ceac240d328317354970e
tahreemzafar2006-coder <tahreemzafar2006@gmail.com>
2025-09-22 21:10:45 +0500

    Add files via upload

---
+++ Tahreem Zafar/task1_variables.py
@@ -0,0 +1,8 @@
+name="Tahreem Zafar"
+age=19
+height=4.8
+is_student=True
+print(name)
+print("Age:",age)
+print("Height:",height)
+print("Student:",is_student)
---
+++ Tahreem Zafar/task2_arithmetic.py
@@ -0,0 +1,9 @@
+a=10
+b=3
+print("Addition:", a + b) 
+print("Subtraction:", a - b) 
+print("Multiplication:", a * b) 
+print("Division (float):", a / b) 
+print("Floor Division (integer result):", a // b) 
+print("Modulus (remainder):", a % b) 
+print("Exponent (power):", a ** b) 
